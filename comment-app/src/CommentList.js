0a896262fa0f09d9ef200f26e70a5314972245a4
JudyC <703433699@qq.com>
2017-07-19 17:19:31 +0800

    Initial commit

---
+++ comment-app/src/CommentList.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Administrator on 2017/7/7 0007.
+ */
+import React,{Component} from 'react'
+import Comment from './Comment'
+import PropTypes from 'prop-types'
+
+class CommentList extends Component{
+  static propTypes={
+    comments:PropTypes.array,
+    onDeleteComment:PropTypes.func
+  }
+
+  static defaultProps={
+    comments:[]
+  }
+
+  handleDeleteComment(index){
+    if(this.props.onDeleteComment){
+      this.props.onDeleteComment(index)
+    }
+  }
+
+  render(){
+    return(
+        <div>
+          {this.props.comments.map((comment,i)=><Comment comment={comment} key={i} index={i} onDeleteComment={this.handleDeleteComment.bind(this)}/>)}
+        </div>
+    )
+  }
+}
+export default CommentList
